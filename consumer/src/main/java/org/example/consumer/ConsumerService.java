80651cc28f118c41b38bc20b451b680fa44b8b56
Vladimir Leushkin <Vleyshkin@yandex.ru>
2023-04-05 21:18:38 +0300

    feat consumer

---
+++ consumer/src/main/java/org/example/consumer/ConsumerService.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.model.Message;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerService {
+
+    @KafkaListener(topics = "messages", groupId = "message_group_id")
+    public void consume(Message message){
+        System.out.println("Consuming the message: " + message);
+    }
+}
---
+++ consumer/src/main/java/org/example/model/Message.java
@@ -0,0 +1,13 @@
+package org.example.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Message {
+    String message;
+    Integer age;
+}
